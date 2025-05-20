365418245cf18fa35e8862563c2b859f596e98e0
Bailey <baileylockheart@gmail.com>
2025-05-19 22:19:38 -0700

    HERE WE GO

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-odule.exports = function (eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
     // This will stop the default behaviour of foo.html being turned into foo/index.html
     eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
